86744507d67ef2d3bebd77409459bf64952201a3
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-03-27 20:17:11 -0700

    Now loading a list of available pdf files onto the front-end and styled inputs for it

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+from os import listdir
+
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path="/static")
+pdfs = filter(lambda file: file.endswith(".pdf"), listdir("src/pdfs"))
+pdfs = list(map(lambda file: file[:-4], pdfs))
+pdfs.sort()
+print(pdfs)
 
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	return render_template("index.html", pdf_list=pdfs)
 
 
 if __name__ == "__main__":
