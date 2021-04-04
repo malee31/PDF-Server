b7936fc04fcbd3993d52944db59b35a1bacbc5a8
Marvin Lee <43216761+malee31@users.noreply.github.com>
2021-04-03 18:29:01 -0700

    Error handling and error modal

--- static/scripts.js
+++ static/scripts.js
@@ -4,6 +4,13 @@ const uploadPDFInput = document.getElementById("pdf-upload");
 const pageRangeInput = document.getElementById("page-range");
 const form = document.getElementById("clip");
 
+const errorBox = document.getElementById("error-box");
+const errorTitle = document.getElementById("error-box-title");
+const errorText = document.getElementById("error-box-text");
+document.getElementById("error-box-dismiss").addEventListener("click", () => {
+	errorBox.style.display = "none";
+});
+
 selectPDFInput.addEventListener("change", e => {
 	if(e.target.value === "Custom Upload") {
 		console.log("Custom Upload Selected");
@@ -19,7 +26,7 @@ uploadPDFInput.addEventListener("change", e => {
 	const uploadText = document.getElementById("upload-text")
 	if(files.length > 0 && files[0].name) uploadText.innerText = files[0].name;
 	else uploadText.innerText = "Upload a PDF";
-})
+});
 
 const allowedKeys = "1234567890-,";
 pageRangeInput.addEventListener("keypress", e => {
@@ -28,13 +35,30 @@ pageRangeInput.addEventListener("keypress", e => {
 	// TODO: Add a prompt saying that only specific characters are allowed
 });
 
+function displayError(title, message) {
+	errorBox.style.display = "";
+	errorTitle.innerText = title;
+	errorText.innerText = message;
+	// console.error(message);
+}
+
 form.addEventListener("submit", e => {
 	e.preventDefault();
 	const data = new FormData();
 	// TODO: Check if inputs are filled
-	if(selectPDFInput.value === "Custom Upload") data.append("uploadedPDF", uploadPDFInput.files[0]);
-	else data.append("selectedPDF", selectPDFInput.value);
-	data.append("pageRange", pageRangeInput.value);
+	if(selectPDFInput.value === "Custom Upload") {
+		if(uploadPDFInput.files.length === 0) {
+			displayError("No File Uploaded", "Attach a PDF and resubmit to continue");
+			return;
+		}
+		data.append("uploadedPDF", uploadPDFInput.files[0]);
+	} else data.append("selectedPDF", selectPDFInput.value);
+	const range = pageRangeInput.value.replace(/[^0-9,-]/g, "");
+	if(range.length === 0) {
+		displayError("Invalid Page Range", "Enter a page range in the form of comma-separated numbers or number ranges\n(Example: 1,3-5,10)");
+		return;
+	}
+	data.append("pageRange", pageRangeInput.value.replace(/[^0-9,-]/g, ""));
 
 	const req = new XMLHttpRequest();
 	req.addEventListener("load", () => {
@@ -43,7 +67,10 @@ form.addEventListener("submit", e => {
 			window.location.href = req.responseText;
 		} else console.warn(`Irregular Status: ${req.statusText}`);
 	});
-	req.addEventListener("error", console.error);
+	req.addEventListener("error", err => {
+		console.error(err);
+		displayError(`Error ${err.status}`, req.statusText);
+	});
 	req.open("POST", `${window.location.origin}/submit`, true);
 	req.send(data);
 });
